c6a261b1b14328e9ab925a23e071ab4110fe2ce0
josebarr03 <jose.barrios@uniat.edu.mx>
2023-05-30 07:22:39 -0600

    el juego ya ve quien gana

--- TicTacToeGame/TicTacToeGame.cpp
+++ TicTacToeGame/TicTacToeGame.cpp
@@ -24,8 +24,11 @@ int main()
 
     string malo = "soyMalo";
     unsigned int turnoJugador = 1;
+    int winner = 0;
+    bool casillaValida = false;
+    bool hayEspacio = false;
 
-    while (true)
+    while (winner == 0)
     {
         system("cls");
         cout << "Referencia de casillas" << endl;
@@ -48,51 +51,71 @@ int main()
         }
         cout << "Jugador #" << turnoJugador << "," << endl;
         cout << "Escriba el numero de la casilla donde quiera poner x" << endl;
-        cin >> casillaJ;
-        if (casillaJ <= 0 || casillaJ > 9)
+        while (casillaValida == false)
         {
-            cout << "Espacio no valido" << endl;
-            cout << "Escriba cualquier cosa para continuar" << endl;
-            cin >> malo;
-        }
-        else
-        {
-            for (int i = 0; i < ROWS; i++)
+            cin >> casillaJ;
+            if (casillaJ <= 0 || casillaJ > 9)
             {
-                for (int j = 0; j < COLUMNS; j++)
+                cout << "Espacio no valido" << endl;
+                /*cout << "Escriba cualquier cosa para continuar" << endl;
+                cin >> malo;*/
+            }
+            else
+            {
+                for (int i = 0; i < ROWS; i++)
                 {
-                    if (tableroShow[i][j] == casillaJ)
+                    for (int j = 0; j < COLUMNS; j++)
                     {
-                        if (tableroGame[i][j] == ' ')
+                        if (tableroShow[i][j] == casillaJ)
                         {
-                            if (turnoJugador == 1)
+                            if (tableroGame[i][j] == ' ')
                             {
-                                tableroGame[i][j] = 'X';
-                                turnoJugador = 2;
+                                if (turnoJugador == 1)
+                                {
+                                    tableroGame[i][j] = 'X';
+                                    //turnoJugador = 2;
+                                    casillaValida = true;
+                                }
+                                else
+                                {
+                                    tableroGame[i][j] = 'O';
+                                    //turnoJugador = 1;
+                                    casillaValida = true;
+                                }
                             }
                             else
                             {
-                                tableroGame[i][j] = 'O';
-                                turnoJugador = 1;
+                                cout << "Espacio ya ocupado" << endl;
+                                /*cout << "Escriba cualquier cosa para continuar" << endl;
+                                cin >> malo;*/
                             }
-                        }
-                        else
-                        {
-                            cout << "Espacio ya ocupado" << endl;
-                            cout << "Escriba cualquier cosa para continuar" << endl;
-                            cin >> malo;
                         }
                     }
                 }
             }
+        }
+
+        //Aqui checa si ya acabo el juego
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                if (tableroGame[i][j] == ' ')
+                {
+                    hayEspacio = true;
+                }
+            }
+        }
+        if (hayEspacio == false)
+        {
+            winner = 3;
         }
         //Aqui checa las columnas
         for (int i = 0; i < ROWS; i++)
         {
             if (tableroGame[i][0] == tableroGame[i][1] && tableroGame[i][0] == tableroGame[i][2] && tableroGame[i][0] != ' ')
             {
-                cout << "Ganaste" << endl;
-                cin >> malo;
+                winner = turnoJugador;
             }
         }
         //Aqui las filas
@@ -100,10 +123,57 @@ int main()
         {
             if (tableroGame[0][i] == tableroGame[1][i] && tableroGame[0][i] == tableroGame[2][i] && tableroGame[0][i] != ' ')
             {
-                cout << "Ganaste" << endl;
-                cin >> malo;
+                winner = turnoJugador;
             }
         }
+        //Aqui la diagonal
+        if (tableroGame[0][0] == tableroGame[1][1] && tableroGame[0][0] == tableroGame[2][2] && tableroGame[0][0] != ' ')
+        {
+            winner = turnoJugador;
+        }
+        //Aqui la diagonal invertida
+        if (tableroGame[0][2] == tableroGame[1][1] && tableroGame[0][2] == tableroGame[2][0] && tableroGame[0][2] != ' ')
+        {
+            winner = turnoJugador;
+        }
+
+
+
+        //Orden
+        casillaValida = false;
+        hayEspacio = false;
+        if (turnoJugador == 1)
+        {
+            turnoJugador = 2;
+        }
+        else
+        {
+            turnoJugador = 1;
+        }
+    }
+    system("cls");
+    if (winner == 3)
+    {
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                cout << "| " << tableroGame[i][j] << " ";
+            }
+            cout << "|" << endl;
+        }
+        cout << "Nadie ha ganado" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLUMNS; j++)
+            {
+                cout << "| " << tableroGame[i][j] << " ";
+            }
+            cout << "|" << endl;
+        }
+        cout << "El jugador " << winner << " ha ganado" << endl;
     }
 }
-
